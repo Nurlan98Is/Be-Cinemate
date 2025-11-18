1979f05ec9ca88604eb48388ae0ff65f5c23ff2e
Nurlan Iskakov <iskakov9821@outlook.com>
2025-11-18 23:48:56 +0700

    create static files

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,18 @@
 import express from 'express';
 import router from './routes';
-
+import path from 'path';
 
 const PORT = 8080
 
 const app = express()
 
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+  });
+
+  
 app.use(router)
 
 app.listen(PORT, () => {
