86d1a3e2457c2dfed1fb010e69ef5edf72abe788
Nurlan Iskakov <iskakov9821@outlook.com>
2025-12-25 18:28:32 +0700

    update dist

--- dist/app.js
+++ dist/app.js
@@ -10,7 +10,7 @@ const cors_1 = __importDefault(require("cors"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)({
-    origin: ["http://localhost:5173", "https://cinemate-phi.vercel.app"],
+    origin: ["http://localhost:5173", "https://cinemate-taupe.vercel.app"],
     credentials: true
 }));
 app.use(express_1.default.json());
